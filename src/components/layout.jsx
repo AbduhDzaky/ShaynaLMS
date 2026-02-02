16c3d747d387cf54d8197ff00124b301f4a66b7f
Abduhdzaky <abduhdzaky000@gmail.com>
2026-02-02 20:06:41 +0800

    ShaynaLMS new page and components

---
+++ src/components/layout.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+import Sidebar from "./sidebar";
+import Header from "./header";
+import Courses from "../pages/ManagerHome/Courses";
+import Students from "../pages/ManagerHome/students";
+
+export default function LayoutDashoard() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,7 @@ import ManagerHome from "../pages/ManagerHome";
 import SignInPage from "../pages/Signin";
 import SignUpPage from "../pages/Signup";
 import SuccessCheckoutPage from "../pages/SuccessCheckout";
+import LayoutDashoard from "../components/layout";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,16 @@ const router = createBrowserRouter([
   {
     path: "/success-checkout",
     element: <SuccessCheckoutPage />,
+  },
+  {
+    path: "/manager",
+    element: <LayoutDashoard />,
+    children: [
+      {
+        index: true,
+        element: <ManagerHome />,
+      },
+    ],
   },
 ]);
 
